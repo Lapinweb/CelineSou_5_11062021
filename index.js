f847750dc1fd8785ce04de6937ef0fb35cf1e557
Céline <celinesou77@gmail.com>
2021-06-14 21:37:39 +0200

    création page js, ajout id dans l'url produit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 createListOfItems()
 
-
+//Créer la liste des produits automatiquement
 async function createListOfItems(){
     const items = await getItems();
     for (item of items){
@@ -8,6 +8,7 @@ async function createListOfItems(){
     }
 }
 
+//Récupérer le tableau contenant toutes les données des produit grâce à une requête GET
 function getItems() {
     return fetch("http://localhost:3000/api/teddies")
         .then(function(res){
@@ -23,21 +24,26 @@ function getItems() {
         })
 }
 
-
+//Ajouter des éléments aux HTML pour créer une carte avec toutes les infos du produit
 function displayItem(item){
     const imageUrl = item.imageUrl;
     const name = item.name;
-    const price = item.price/100;
+    const price = item.price/100; //convertie le prix en euros
     const description = item.description;
+    const id = item._id;
 
+    //Récupérer la template dans le code HTML
     const itemTemplate = document.getElementById("item-card");
     const itemClone = document.importNode(itemTemplate.content, true);
 
+    //Modifier la template avec des variables
     const image = itemClone.getElementById("image");
     image.setAttribute("src", imageUrl);
     itemClone.getElementById("name").textContent = name;
-    itemClone.getElementById("price").textContent = price.toFixed(2) + "€";
+    itemClone.getElementById("price").textContent = price.toFixed(2) + "€"; //rajoute 2 décimales qui représentent les centimes
     itemClone.getElementById("description").textContent = description;
+    itemClone.getElementById("item-link").setAttribute("href", "produit.html?id=" + id); //rajoute le paramètre id derrière l'URL vers la page produit
 
+    //Ajouter un élément basé sur la template
     document.getElementById("items-list").appendChild(itemClone);
 }
