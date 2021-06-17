938b1df68db583f4e7a9ef28349e665991411f23
Céline <celinesou77@gmail.com>
2021-06-17 23:50:28 +0200

    mise en forme formulaire

--- js/panier.js
+++ js/panier.js
@@ -1,5 +1,3 @@
-////////////////////////////////////////Afficher la liste des produits dans le panier et son prix total///////////
-
 
 createBasketList();
 
@@ -8,18 +6,24 @@ async function createBasketList() {
     let basketContent = await getBasketContent();
     console.log("basketContent :", basketContent)
 
-    if (basketContent === 0) {
+    if (basketContent.length === 0) {
         displayEmptyBasket();
+        hideForm();
 
-    }else{
+    }else{       
         for (let item of basketContent) {
             displayBasketItem(item);
         }
 
         let totalPrice = calculateTotalPrice(basketContent);
-        displayTotalPrice(totalPrice);          
+        displayTotalPrice(totalPrice);
+        
+        //La fonction est ajouté après que les boutons un article soient créé pour être fonctionnelle
+        buttonRemoveItem(basketContent);
     }
 }
+
+/////////////////////////////////////////////// Afficher la liste des produits dans le panier et son prix total /////////////////////////////////////////////////////////////////////
 
 //Afficher un message si le panier est vide
 function displayEmptyBasket() {
@@ -32,7 +36,7 @@ function displayEmptyBasket() {
 //Récupérer le contenu du panier dans le localStorage
 function getBasketContent() {
     if (!localStorage.getItem("basketContent")) {
-        return 0;
+        return [];
     }else{
         let basketContent = JSON.parse(localStorage.getItem("basketContent"));
 
@@ -83,3 +87,45 @@ function displayTotalPrice(price) {
 
 
 
+///////////////////////////////////////////////////////////// Retirer les produits du panier /////////////////////////////////////////////////////////////////////////////
+
+//Bouton supprimer un article, la fonction doit être appelé après la création des boutons pour être fonctionnelle 
+function buttonRemoveItem(basketContent) {
+    //Récupérer le tableau de tout les éléments des boutons
+    const removeButtons = document.getElementsByClassName("btn-remove"); 
+
+    //une boucle pour écouter tous les événements de click du boutons
+    for (let i = 0; i < removeButtons.length; i++) { //i = indice du produit dans le tableau removeButtons mais aussi basketContent
+        removeButtons[i].addEventListener("click", function(event){
+            //Enlever l'objet du tableau, avec i qui nous donne l'indice
+            basketContent.splice(i, 1);
+
+            //Renvoyer le tableau à localStorage
+            localStorage.setItem("basketContent", JSON.stringify(basketContent)); 
+
+            //Actualiser la page
+            location.reload();
+        })
+    }    
+}
+
+//Bouton vider le panier
+document.getElementById("btn-emptybasket").addEventListener("click", function(){
+    //Vider le localStorage
+    localStorage.clear("basketContent");
+
+    //Actualiser la page
+    location.reload();
+
+    //Message panier vide
+    alert("Le panier a été vidé.");
+})
+
+
+///////////////////////////////////////////////////////////////////// Formulaire de contact ///////////////////////////////////////////////////////////////////////////////////////
+
+//Cacher le formulaire si le panier est vide
+function hideForm(){
+    const form = document.getElementById("form");
+    form.classList.add("d-none");
+}
